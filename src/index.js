8d7b4302cebcb3df88c1f9c63c6320365009137e
aanamarshad <FA19-BSE-084@cuilahore.edu.pk>
2023-11-28 12:04:39 +0500

    commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TaxForm from './components/TaxForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TaxForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
